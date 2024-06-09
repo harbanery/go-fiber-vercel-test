9c06bdd6f2188e647baeb432c1aa5ff3e4099d62
harbanery <ryusuf05@gmail.com>
2024-06-09 13:48:26 +0700

    fixing vercel

--- api/index.go
+++ api/index.go
@@ -2,10 +2,27 @@ package handler
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type Product struct {
+	gorm.Model
+	Name        string  `json:"name" validate:"required"`
+	Price       float64 `json:"price" validate:"required,gt=0"`
+	Stock       int     `json:"stock" validate:"required,gt=0"`
+	Image       string  `json:"image" validate:"required"`
+	Size        uint    `json:"size" validate:"required,gt=0"`
+	Color       string  `json:"color" validate:"required,iscolor"`
+	Rating      uint    `json:"rating" gorm:"default:0"`
+	Description string  `json:"description" validate:"required"`
+	CategoryID  uint    `json:"category_id" validate:"required"`
+	SellerID    uint    `json:"seller_id" validate:"required"`
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	r.RequestURI = r.URL.String()
@@ -19,13 +36,32 @@ func handler() http.HandlerFunc {
 	// }
 	app := fiber.New()
 
-	// configs.InitDB()
-	// helpers.Migration()
-	// routes.Router(app)
-
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, This is API for Blanja by Raihan Yusuf from Codecraft")
 	})
 
+	app.Get("/products", getAllProducts)
+
 	return adaptor.FiberApp(app)
+}
+
+func getAllProducts(c *fiber.Ctx) error {
+
+	url := os.Getenv("URL")
+	var err error
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	var products []*Product
+	results := db.Find(&products)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":     "success",
+		"statusCode": 200,
+		"data":       results,
+	})
 }
