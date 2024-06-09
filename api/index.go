e3e6e18e07d37626ae531b0ea727c8fc8335fd70
harbanery <ryusuf05@gmail.com>
2024-06-09 12:19:43 +0700

    initial commit

---
+++ api/index.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	r.RequestURI = r.URL.String()
+
+	handler().ServeHTTP(w, r)
+}
+
+func handler() http.HandlerFunc {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	return adaptor.FiberApp(app)
+}
